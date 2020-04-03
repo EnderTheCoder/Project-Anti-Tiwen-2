58a661d89eba7e0455760c845be1b095b909549b
ender <1991455223@qq.com>
2020-04-03 22:01:45 +0800

    移除了ip限制

--- api.php
+++ api.php
@@ -15,11 +15,11 @@ if (isEmpty($_POST['name']) || isEmpty($_POST['class']) || isEmpty($_POST['grade
     $return->retMsg('emptyParam');
 
 //判定是否在白名单内
-$mysql->bind_query('SELECT * FROM main_white_list WHERE ip_addr = ?', $_SERVER['REMOTE_ADDR']);
-if (!$mysql->fetchLine('invite_ip')) {
-    $return->retMsg('noResult', '您的ip不在本站的白名单中,请将以下链接分享给一位新用户打开即可立即解锁 
-    http://ender.yishugou.shop/Project-Anti-Tiwen-2/?i=' . $mysql->fetchLine('id'));
-}
+//$mysql->bind_query('SELECT * FROM main_white_list WHERE ip_addr = ?', $_SERVER['REMOTE_ADDR']);
+//if (!$mysql->fetchLine('invite_ip')) {
+//    $return->retMsg('noResult', '您的ip不在本站的白名单中,请将以下链接分享给一位新用户打开即可立即解锁
+//    http://ender.yishugou.shop/Project-Anti-Tiwen-2/?i=' . $mysql->fetchLine('id'));
+//}
 
 $mysql->bind_query('SELECT * FROM name_list WHERE name = ?', $_POST['name']);
 
