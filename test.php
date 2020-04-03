b970f760e07a2329685241d11f0f0271d01ab2a3
ender <1991455223@qq.com>
2020-04-03 18:33:42 +0800

    尝试修复问题

--- test.php
+++ test.php
@@ -1,9 +1,23 @@
 <?php
 
-$content = null;
-exec("curl 'http://gl.lyd24zx.com/xsyqbb/yqtwsb_add.asp' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: http://gl.lyd24zx.com' -H 'Connection: keep-alive' -H 'Referer: http://gl.lyd24zx.com/xsyqbb/yqtwsb.asp?xm1=%D6%DC%F0%D8&bj1=4%B0%E0&nj1=%B8%DF%D2%BB' -H 'Cookie: hehejt=yhjl=checked&nj=%B8%DF%D2%BB&mima=g1004&bj=4&username=%D6%DC%F0%D8; ASPSESSIONIDSSQTBBCT=HIFKMAGCEMKMMFPIKBHFMMGO' -H 'Upgrade-Insecure-Requests: 1' --data 'xm=%D6%DC%F0%D8&nj=%B8%DF%D2%BB&bj=4%B0%E0&jtzz=%B5%D8%D6%B7%D2%FE%B2%D8&sfcx=%B7%F1&cxdd=&fhsj=&glts=&gldd=&mrtw=36.5&xg=%CC%E1%BD%BB'", $content);
-$final = '';
-for ($i = 0; $i < count($content); $i++) {
-    $final .= $content[$i] . "\n";
-}
-echo iconv("gb2312", "utf-8", $final);
+$cookie_file = dirname(__FILE__).'/cookie.txt';
+//$cookie_file = tempnam("tmp","cookie");
+//先获取cookies并保存
+$url = "http://gl.lyd24zx.com/xsyqbb/xsyq.asp";
+$ch = curl_init($url); //初始化
+curl_setopt($ch, CURLOPT_HEADER, 0); //不返回header部分
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //返回字符串，而非直接输出
+curl_setopt($ch, CURLOPT_COOKIEJAR,  $cookie_file); //存储cookies
+curl_exec($ch);
+curl_close($ch);
+
+//使用上面保存的cookies再次访问
+
+$url = "http://gl.lyd24zx.com/xsyqbb/yqtwsb_add.asp";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file); //使用上面获取的cookies
+$response = curl_exec($ch);
+curl_close($ch);
+echo $response;
