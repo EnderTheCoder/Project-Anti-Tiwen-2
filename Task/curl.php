fced7badc80ff3a06a4d1fe56c10e94138db102d
ender <1991455223@qq.com>
2020-04-09 14:30:36 +0800

    增加延时补偿

--- Task/curl.php
+++ Task/curl.php
@@ -14,11 +14,10 @@ function get_together($arr)
 $mysql = new mysql_core();
 
 while (1) {
+    $timer_a = time();
     $result = $mysql->bind_query('SELECT * FROM name_list');
     for ($i = 0; $i < count($result); $i++) {
-
         $password = 'g';
-
         switch ($result[$i]['grade']) {
             case '高一':
             {
@@ -54,7 +53,6 @@ while (1) {
         exec("curl 'http://gl.lyd24zx.com/xsyqbb/xsyq_qr.asp' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: http://gl.lyd24zx.com' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: http://gl.lyd24zx.com/xsyqbb/xsyq.asp' -b @cookie.txt -H 'Upgrade-Insecure-Requests: 1' --data 'username=" . $post_fields['name'] . "&nj=" . $post_fields['grade'] . "&bj=" . $class . "&mima=" . $password . "&qr2=%C8%B7%B6%A8'", $arr[1]);
         echo get_together($arr[1]);
         if (count($arr[1]) != 9 || iconv("gb2312", "utf-8", $arr[1][5]) != "   alert(\"登陆成功！\")") {
-            echo "errrrrrrrrrrrrrr\n";
             $mysql->bind_query('INSERT INTO main_logs (name, return_value, time) VALUES (?,?,?)', array(
                 1 => $result[$i][0],
                 2 => get_together($arr[1]),
@@ -68,7 +66,6 @@ while (1) {
         echo get_together($arr[3]);
 
         if (count($arr[3]) != 6 || !iconv("gb2312", "utf-8", $arr[3][3]) == "   alert(\"上报内容已经保存，返回\")") {
-            echo "errrrrrrrrrrrrrr\n";
             $mysql->bind_query('INSERT INTO main_logs (name, return_value, time) VALUES (?,?,?)', array(
                 1 => $result[$i][0],
                 2 => get_together($arr[3]),
@@ -76,5 +73,6 @@ while (1) {
             ));
         }
     }
-    sleep(7200);
+    $timer_b = time();
+    sleep(7200 - $timer_b + $timer_a);
 }
