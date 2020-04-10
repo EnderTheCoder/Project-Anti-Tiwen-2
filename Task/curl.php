afec3d4dde6f27fd312a99394add0209be6b86ad
ender <1991455223@qq.com>
2020-04-10 14:38:53 +0800

    修改了前端,修改了轮询时间

--- Task/curl.php
+++ Task/curl.php
@@ -64,7 +64,6 @@ while (1) {
 //        echo get_together($arr[2]);
         exec("curl 'http://gl.lyd24zx.com/xsyqbb/yqtwsb_add.asp' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: http://gl.lyd24zx.com' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: http://gl.lyd24zx.com/xsyqbb/yqtwsb.asp?xm1=" . $post_fields['name'] . "&bj1=" . $post_fields['class'] . "&nj1=" . $post_fields['grade'] . "' -b @cookie.txt -H 'Upgrade-Insecure-Requests: 1' --data 'xm=" . $post_fields['name'] . "&nj=" . $post_fields['grade'] . "&bj=" . $post_fields['class'] . "&jtzz=%B5%D8%D6%B7%D2%FE%B2%D8&sfcx=%B7%F1&cxdd=&fhsj=&glts=&gldd=&mrtw=" . $post_fields['tem'] . "&xg=%CC%E1%BD%BB'", $arr[3]);
         echo get_together($arr[3]);
-
         if (count($arr[3]) != 6 || !iconv("gb2312", "utf-8", $arr[3][3]) == "   alert(\"上报内容已经保存，返回\")") {
             $mysql->bind_query('INSERT INTO main_logs (name, return_value, time) VALUES (?,?,?)', array(
                 1 => $result[$i][0],
@@ -74,5 +73,5 @@ while (1) {
         }
     }
     $timer_b = time();
-    sleep(7200 - $timer_b + $timer_a);
+    sleep(3600 - $timer_b + $timer_a);
 }
